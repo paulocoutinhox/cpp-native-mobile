9d5d22167708c26d7e0934025f5cdd3aae16c491
Paulo Coutinho <paulocoutinhox@gmail.com>
2022-10-12 07:36:38 -0300

    project update

--- cxx/src/main.cpp
+++ cxx/src/main.cpp
@@ -20,7 +20,7 @@ struct MappingItem
     std::function<const std::string(const std::string &)> executor;
     std::function<std::string(TypeWrapper &)> target;
 
-    MappingItem(std::string name, const std::function<std::string(TypeWrapper &)> &target, const std::function<const std::string(const std::string &)> &executor)
+    MappingItem(const std::string &name, const std::function<std::string(TypeWrapper &)> &target, const std::function<const std::string(const std::string &)> &executor)
     {
         this->name = name;
         this->target = target;
